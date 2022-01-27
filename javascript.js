965ec64b8ecc41e0abc565ef07dd8762e7143c5d
skavenRunner <Matt.Jaulo@gmail.com>
2022-01-27 08:04:41 -0300

    Added a way to display books

--- javascript.js
+++ javascript.js
@@ -1,4 +1,5 @@
-let myLibrary = [];
+let myLibrary = [{title:"tituloPrueba", author:"autorPrueba", pages:"123", read:"Leido"},
+ {title:"tituloPrueba2", author:"autorPrueba2", pages:"1232", read:"Leido2"}];
 
 function book(title, author, pages, read) {
     this.title = title;
@@ -10,6 +11,36 @@ function book(title, author, pages, read) {
     }
 };
 
-function addBookToLibrary() {
-
+function addBookToLibrary(title, author, pages, read) {
+    let currentBook = new book(title, author, pages, read);
+    myLibrary.push(currentBook);
+    updateBookDisplay();
 };
+
+function updateBookDisplay() {
+    const grid = document.querySelector(".grid");
+    const sampleBook = document.querySelector(".book.sample");
+
+    while (grid.children.length > 1) {
+        grid.removeChild(grid.lastChild);
+    }
+
+    myLibrary.forEach(book => {
+        const currentBook = sampleBook.cloneNode(true);
+        currentBook.classList.remove("sample");
+        const title = currentBook.querySelector(".title");
+        const author = currentBook.querySelector(".author");
+        const pages = currentBook.querySelector(".pages");
+        const read = currentBook.querySelector(".read");
+
+        title.textContent = book.title;
+        author.textContent = book.author;
+        pages.textContent = book.pages;
+        read.textContent = book.read;
+
+
+        grid.appendChild(currentBook);
+    });
+    
+    
+}
